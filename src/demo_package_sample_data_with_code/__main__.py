707632787b6b883490b6920ee9a64e1543c90140
Jim Ratliff <jim@jim-ratliff.name>
2022-05-25 15:13:49 -0700

    Re-refactored __main__.py to put main() into my_module.py

--- src/demo_package_sample_data_with_code/__main__.py
+++ src/demo_package_sample_data_with_code/__main__.py
@@ -2,36 +2,14 @@
 Reads data from packaged resources for π, e, and the meaning of life.
 
 Entry point for package.
-See, e.g., https://docs.python.org/3/library/__main__.html
+See, e.g., https://docs.python.org/3/library/__main__.html#main-py-in-python-packages
 """
 
-import os
-
-from . import constants
-from . my_module import check_CLI_for_user_input
-from . my_module import print_value_from_resource
-
-
-def main():
-    print("I am here, in __main__.py.")
-    print("\n" + 15*"# " + "\n")
-
-    print(f"\nThe current working directory: {os.getcwd()} \n")
-
-    # Checks for command line argument 
-    user_contribution = check_CLI_for_user_input()
-    if len(user_contribution) == 0:
-        print("The user declined to share any knowledge. 🙁\n")
-    else:
-        user_wisdom = " ".join(user_contribution)
-        print(f"The user chose to share: “{user_wisdom}”\n")
-
-    print_value_from_resource("π", constants.PACKAGENAME_PI, constants.FILENAME_PI)
-    print_value_from_resource("e", constants.PACKAGENAME_E, constants.FILENAME_E)
-    print("\nPlease don’t be concerned when you see the following error message. It’s expected.")
-    print_value_from_resource("Meaning of life", constants.PACKAGENAME_MOL, constants.FILENAME_MOL)
-    print("\n" + 15*"* " + "\n")
-
+# Note the the following import appears not to work if you attempt to run my_module.py directly
+# with `python src/demo_package_sample_data_with_code/my_module.py`, returning the following error:
+#     ImportError: attempted relative import with no known parent package
+# However, I plan to run this from within a package, in which case it works fine.
+from . my_module import main
 
 if __name__ == "__main__":
     main()
--- src/demo_package_sample_data_with_code/my_module.py
+++ src/demo_package_sample_data_with_code/my_module.py
@@ -8,6 +8,37 @@ from importlib.resources import files
 
 from . import constants
 
+def main():
+    print("I am here, in __main__.py.")
+    print("\n" + 15*"# " + "\n")
+
+    # Checks for command line argument 
+    user_contribution = check_CLI_for_user_input()
+    if len(user_contribution) == 0:
+        print("The user declined to share any knowledge. 🙁\n")
+    else:
+        user_wisdom = " ".join(user_contribution)
+        print(f"The user chose to share: “{user_wisdom}”\n")
+
+    print_value_from_resource("π", constants.PACKAGENAME_PI, constants.FILENAME_PI)
+    print_value_from_resource("e", constants.PACKAGENAME_E, constants.FILENAME_E)
+    print("\nPlease don’t be concerned when you see the following error message. It’s expected.")
+    print_value_from_resource("Meaning of life", constants.PACKAGENAME_MOL, constants.FILENAME_MOL)
+    print("\n" + 15*"* " + "\n")
+
+    # For the rationale for `return 0`, see
+    # https://docs.python.org/3/library/__main__.html#packaging-considerations
+    # (`main` functions are often used to create command-line tools by specifying them as entry points for console
+    # scripts. When this is done, pip inserts the function call into a template script, where the return value of `main`
+    # is passed into `sys.exit()`, which expects a return value. By proactively specifying this return value, the
+    # module will have the same behavior when run directly (i.e., `python my_module.py`) as it will as a console script
+    # entry point in a pip-installable package.)
+    # (That said, even without this, I can't run my_module.py directly, because it chokes on the import statement
+    # `from . my_module import main` with the error message:
+    #    ImportError: attempted relative import with no known parent package
+    # So, this return statement may not have an actual benefit, but it certainly does no harm either.
+    return 0
+
 
 def check_CLI_for_user_input():
     parser = argparse.ArgumentParser()
